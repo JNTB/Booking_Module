7f57f2c8805a316abb7d366b0437b949d42b22a4
Jane Lee <sohyunlee53@gmail.com>
2018-10-25 08:58:45 -0700

    placeholder renderin gon page, working on basic structures

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,1 +1,33 @@
+import React from 'react';
+// import styled from 'styled-components';
+// import style from '../dist/style.css';
+import styled from 'styled-components';
 
+const AppComponent = styled.div `
+  background-color: #44014C;
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <AppComponent>
+        <div>
+          <h1>Booking Module</h1>
+        </div>
+      </AppComponent>
+   
+    )
+  }
+}
+
+export default App;
+
+//how many components do I need?
+  //Price per night + number of reviews + star rating
+  //Dates picker - stateful
+  //Guests picker - stateful
+  //Book button - stateless
